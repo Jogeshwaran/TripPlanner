1be9dc9bad6db3ff6873a3a5673cffc0278056fe
Jogeshwaran <64849330+Jogeshwaran@users.noreply.github.com>
2026-02-17 18:51:30 +0530

    Remove API_KEY from Constants.js

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,3 +1,1 @@
-const API_KEY = "AIzaSyCjDF32s_GrC9aL8yHPLGqKbB3wpmqmYjo"
 
-export const API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY
